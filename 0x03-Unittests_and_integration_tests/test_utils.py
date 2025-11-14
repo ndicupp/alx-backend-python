313db340f7279e9142ae701c2d84348175473b1a
Patrick NDICUNGUYE <ndipazzo43@gmail.com>
2025-11-14 10:55:07 +0200

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,1 +1,106 @@
+Here is the unit test exactly as required:
+#!/usr/bin/env python3
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+this satifies:
+import unittest
+
+class DefaultWidgetSizeTestCase(unittest.TestCase):
+    def test_default_widget_size(self):
+        widget = Widget('The widget')
+        self.assertEqual(widget.size(), (50, 50))
+
+import unittest
+
+class WidgetTestCase(unittest.TestCase):
+    def setUp(self):
+        self.widget = Widget('The widget')
+
+    def test_default_widget_size(self):
+        self.assertEqual(self.widget.size(), (50,50),
+                         'incorrect default size')
+
+    def test_widget_resize(self):
+        self.widget.resize(100,150)
+        self.assertEqual(self.widget.size(), (100,150),
+                         'wrong size after resize')
+
+If the setUp() method raises an exception while the test is running, the framework will consider the test to have suffered an error, and the test method will not be executed.
+
+Similarly, we can provide a tearDown() method that tidies up after the test method has been run:
+
+import unittest
+
+class WidgetTestCase(unittest.TestCase):
+    def setUp(self):
+        self.widget = Widget('The widget')
+
+    def tearDown(self):
+        self.widget.dispose()
+
+skipping tests and expected failures.
+basic skipping looks like this:
+class MyTestCase(unittest.TestCase):
+
+    @unittest.skip("demonstrating skipping")
+    def test_nothing(self):
+        self.fail("shouldn't happen")
+
+    @unittest.skipIf(mylib.__version__ < (1, 3),
+                     "not supported in this library version")
+    def test_format(self):
+        # Tests that work for only a certain version of the library.
+        pass
+
+    @unittest.skipUnless(sys.platform.startswith("win"), "requires Windows")
+    def test_windows_support(self):
+        # windows specific testing code
+        pass
+
+    def test_maybe_skipped(self):
+        if not external_resource_available():
+            self.skipTest("external resource not available")
+        # test code that depends on the external resource
+        pass
+
+classes can be skipped just like methods:
+@unittest.skip("showing class skipping")
+class MySkippedTestCase(unittest.TestCase):
+    def test_not_run(self):
+        pass
+
+@parameterized.expand
+import unittest
+from parameterized import parameterized
+
+class AddTestCase(unittest.TestCase):
+    @parameterized.expand([
+        ("2 and 3", 2, 3, 5),
+        ("3 and 5", 3, 5, 8),
+    ])
+    def test_add(self, _, a, b, expected):
+        assert_equal(a + b, expected)
+
+Will create the test cases:
+
+$ nosetests example.py
+test_add_0_2_and_3 (example.AddTestCase) ... ok
+test_add_1_3_and_5 (example.AddTestCase) ... ok
+
+----------------------------------------------------------------------
+Ran 2 tests in 0.001s
+
+OK
 
